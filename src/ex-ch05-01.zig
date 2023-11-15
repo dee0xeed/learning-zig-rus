689555b3a94e40c66cd603a04486b496eb5dfd5a
zed <dee0xeed@gmail.com>
2023-11-15 20:56:43 +0300

    On branch main modified:   src/ch05.md new file:   src/ex-ch05-01.zig

---
+++ src/ex-ch05-01.zig
@@ -0,0 +1,23 @@
+
+const std = @import("std");
+
+pub fn main() void {
+    var user1 = User.init(1, 10);
+    var user2 = User.init(2, 20);
+
+    std.debug.print("User {d} has power of {d}\n", .{user1.id, user1.power});
+    std.debug.print("User {d} has power of {d}\n", .{user2.id, user2.power});
+}
+
+pub const User = struct {
+    id: u64,
+    power: i32,
+
+    fn init(id: u64, power: i32) *User{
+        var user = User{
+            .id = id,
+            .power = power,
+        };
+        return &user;
+    }
+};
